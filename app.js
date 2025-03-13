d26272c9e62ddf21f675aec9b5d116d5f86aaa03
Pakhi-Rajpoot <pakhirajpootmpi@gmail.com>
2025-03-14 04:12:57 +0530

    port setup

--- app.js
+++ app.js
@@ -41,4 +41,8 @@ app.get("/", (req, res)=>{
     res.send("hey");
 });
 
-app.listen(3000);
+// app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
