8c04b99078073dab66be6bf46983d287fb51c7f2
Pakhi-Rajpoot <pakhirajpootmpi@gmail.com>
2025-03-16 12:03:00 +0530

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,11 +13,15 @@ const { response } = require("express");
 module.exports.registerUser = async function(req, res){
     try{
         let{email, fullname, password} = req.body;
+        if(fullname.length < 3){
+            return res.status(400).json({error:'name must be atleast 3 characters long'})
+        }
 
        let user = await userModel.findOne({email:email})
        if(user){
-        return res.status(401).send("You already have an account, please login")
+          return res.send("You already have an account, please login")
        }
+       
         bcrypt.genSalt(10, function(err,salt){
             bcrypt.hash(password, salt, async function(err, hash){
                 if(err) return res.send(err.message);
@@ -32,7 +36,7 @@ module.exports.registerUser = async function(req, res){
                     let token = generateToken(user);
                     
                     res.cookie("token",token);
-                    res.send("user created successfully")
+                    res.send("user created successfully. Please login")
 
                 }
             })
